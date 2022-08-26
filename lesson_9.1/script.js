2bbae010103cc206451152b62bade09837b5fb2c
Borislav <borislavutkov1@gmail.com>
2022-08-26 13:46:14 +0500

    make all for first lesson

--- lesson_9.1/script.js
+++ lesson_9.1/script.js
@@ -1,4 +1,24 @@
+"use strict";
 let numberOfFilms = 0;
-numberOfFilms = prompt('Сколько фильмов вы уже просмотрели', 0);
-console.log(numberOfFilms);
+//numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', 0);
+// console.log(numberOfFilms);
 
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+//_____________________________________________________
+
+let a = prompt("Что смотрели недавно?",""),
+    b = +prompt("Ваша оценка фильму",""),
+    c = prompt("Что смотрели недавно?",""),
+    d = +prompt("Ваша оценка фильму","");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB.movies);
